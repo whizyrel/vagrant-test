b715f62cf00fc7f20f224d14d2f14f58d5af8bbe
whizyrel <olaleyeisrael@gmail.com>
2022-07-01 12:40:47 +0100

    test vm using vagrant

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vagrant Test
+
+Creates a test VM using Virtualbox. At this time, it uses the default insecure private key.
---
+++ script.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+# cat /home/j117/test_vagrant_box.pub > /home/vagrant/.ssh/authorized_keys
+# cat /home/j117/test_vagrant_box.pub > /home/vagrant/.ssh/test_vagrant_box.pub
+# cat /home/j117/test_vagrant_box.pub > /root/.ssh/authorized_keys
+
+apt-get update
+apt-get install -y apache2
