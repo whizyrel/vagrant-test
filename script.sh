f03127c7d4ae0c02f47c92287ad83bf063b1c7fe
whizyrel <olaleyeisrael@gmail.com>
2022-07-02 03:14:11 +0100

    Fix Shell Provisioner Error Fixes #1

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # cat /home/j117/test_vagrant_box.pub > /home/vagrant/.ssh/authorized_keys
 # cat /home/j117/test_vagrant_box.pub > /home/vagrant/.ssh/test_vagrant_box.pub
